a36b7dbd637af8fbea0a2f26d6eee5513f245248
sakshamnarang131-cmd <sakshamnarang131@gmail.com>
2026-07-16 21:42:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (16.63%)

---
+++ 0027-remove-element/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        n = len(nums)
+        i=0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.remove(nums[i]) 
+            else:
+                i+=1
+                
+        return len(nums)
