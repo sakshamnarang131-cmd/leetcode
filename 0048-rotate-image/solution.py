d404f32a1569dc7e9cf0bf01b273b881469cdf37
sakshamnarang131-cmd <sakshamnarang131@gmail.com>
2026-07-17 21:27:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.7 MB (24.07%)

---
+++ 0048-rotate-image/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        a = []
+        n = len(matrix)
+        for j in range (n):
+            for i in range (n):
+                a.append(matrix[n-1-i][j])
+        k = 0
+        for i in range(n):
+            for j in range(len(matrix[0])):
+                matrix[i][j] = a[k]
+                k +=1
+        return matrix
