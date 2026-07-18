8003385874635d92f9e0afe6bf1b7c62035e94a2
sakshamnarang131-cmd <sakshamnarang131@gmail.com>
2026-07-18 11:06:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.7 MB (20.35%)

---
+++ 2106-find-greatest-common-divisor-of-array/solution.py
@@ -0,0 +1,8 @@
+import fractions
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return fractions.gcd(min(nums), max(nums))
