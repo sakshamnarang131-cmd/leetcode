6472b85caf334b95c49075a845b0bcdb3c41a9be
sakshamnarang131-cmd <sakshamnarang131@gmail.com>
2026-07-20 12:19:33 +0000

    Sync LeetCode submission Runtime - 22 ms (45.57%), Memory - 12.3 MB (88.20%)

---
+++ 1013-fibonacci-number/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        f = [0] * (n+1)
+        if n ==0:
+            return 0
+        f[1] = 1
+        if n == 1:
+            return 1
+        for i in range(2,n+1):
+            f[i] = f[i-1] + f[i-2]
+        return f[n]
