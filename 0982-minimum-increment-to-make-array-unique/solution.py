ccf843a1f7b53af6b49cd2dce3ebba40f42ffbd9
sakshamnarang131-cmd <sakshamnarang131@gmail.com>
2026-07-22 17:00:50 +0000

    Sync LeetCode submission Runtime - 213 ms (24.56%), Memory - 20.6 MB (31.58%)

---
+++ 0982-minimum-increment-to-make-array-unique/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minIncrementForUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        result = 0
+        for i in range(1,len(nums)):
+            if nums[i] <= nums[i-1]:
+                target = nums[i-1] + 1
+                result += target - nums[i]
+                nums[i] = target
+        return result
