f3e750a49495acbe79049b23d8c28e709821a2e3
sakshamnarang131-cmd <sakshamnarang131@gmail.com>
2026-07-29 23:44:11 +0000

    Sync LeetCode submission Runtime - 19 ms (20.69%), Memory - 12.3 MB (93.36%)

---
+++ 0350-intersection-of-two-arrays-ii/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(len(nums1)):
+            if nums1[i] in nums2:
+                nums2.remove(nums1[i])
+                result.append(nums1[i])
+        return result
