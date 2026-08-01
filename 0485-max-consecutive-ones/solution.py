bceae036c549850b78bcb5f250382b110c40b1d8
sakshamnarang131-cmd <sakshamnarang131@gmail.com>
2026-08-01 16:45:03 +0000

    Sync LeetCode submission Runtime - 22 ms (65.82%), Memory - 16.2 MB (8.55%)

---
+++ 0485-max-consecutive-ones/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        max_count = 0
+        for i in range(len(nums)):
+            if nums[i] != 1:
+                count = 0
+            else:
+                count+=1
+            if count> max_count:
+                max_count = count
+        return max_count
