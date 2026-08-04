bd4fc105dfbfaedb221f16baa02091bf1a6317f9
sakshamnarang131-cmd <sakshamnarang131@gmail.com>
2026-08-04 09:32:29 +0000

    Sync LeetCode submission Runtime - 2 ms (51.96%), Memory - 12.5 MB (0.56%)

---
+++ 3778-transform-array-by-parity/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def transformArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            if nums[i]%2 == 0:
+                nums[i] = 0
+            else:
+                nums[i] = 1
+        return sorted(nums)
